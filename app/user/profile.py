9158c926172c9a7b6b3e6673afca1f73edf26d3a
christianmanaog <manaog.scjay@hotmail.com>
2025-05-01 18:11:23 +0800

    check check

--- app/user/profile.py
+++ app/user/profile.py
@@ -8,3 +8,33 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 #login blueprint
 profile = Blueprint("profile", __name__, static_folder="static", template_folder="templates")
+
+@profile.route('/employee/<int:employee_id>', methods=['GET'])
+
+#fetch from database
+def get_employee_details(employee_id):
+
+    #find user record
+    user = WebsiteUsers.query.filter_by(employee_id=employee_id).first()
+
+    if not user:
+        return {"error": "User not found"}, 404
+    
+    #get employee record linked to the user
+    employee = Employee.query.filter_by(employee_id=employee_id).first()
+
+    
+
+    employee_details = {
+        "employee_id": user.employee_id,
+        "first_name": user.first_name,
+        "last_name": user.last_name,
+        "email": user.email,
+        "phone_number": employee.phone_number if employee else None,
+        "home address": employee.home_address if employee else None,
+        "date_hired": employee.date_hired.strftime("%Y-%m-%d") if employee and employee.date_hired else None,
+        "employee_position": employee.employee_position if employee else None,
+        "department_id": employee.department_id if employee else None,
+    }
+        
+    return render_template("to_do.html", employee_details=employee_details)
